bfe1bdb04fff3607ccd82d3c3d50940a1f3a9272
Err1ck <erryck-1996@hotmail.com>
2023-10-31 13:50:13 +0100

    Exercise1

--- exercise-1-primitive-types-start.ts
+++ exercise-1-primitive-types-start.ts
@@ -6,13 +6,13 @@
 
 let country: string = "Italy";
 
-let year = undefined;
+let year: undefined = undefined;
 
 let averageTemperature: number = 23.6;
 
 let visited: boolean = true;
 
-let currency = null;
+let currency: null = null;
 
 // Change the type annotations on these variables so they are correct.
 
